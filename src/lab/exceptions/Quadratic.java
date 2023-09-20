7ff3da5d35dda3266197bbede9100cb504bc6579
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2023-09-19 21:58:26 -0500

    Add code (some old, some new).

---
+++ src/lab/exceptions/Quadratic.java
@@ -0,0 +1,84 @@
+package lab.exceptions;
+
+/**
+ * Quadratic expressions (of the form ax^2 + bx + c).
+ * 
+ * @author Samuel A. Rebelsky
+ * @author YOUR NAME HERE
+ */
+public class Quadratic {
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+  
+  /**
+   * The coefficient of the x^2 component.
+   */
+  double a;
+
+  /**
+   * The coefficient of the x component.
+   */
+  double b;
+
+  /**
+   * The constant.
+   */
+  double c;
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Create a new quadratic.
+   */
+  public Quadratic(double a, double b, double c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  } // Quadratic(double, double, double)
+
+  // +------------------+--------------------------------------------
+  // | Standard Methods |
+  // +------------------+
+
+  /**
+   * Convert to a string.
+   */
+  public String toString() {
+    return a + "x^2 + " + b + "x + " + c;
+  } // toString()
+
+  // +---------+-----------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  /**
+   * Build a string that shows the expression with a particular value.
+   */
+  public String toString(double val) {
+    return a + "*" + val + "^2" + "+" + b + "*" + val + "+" + c;
+  } // toString(double)
+  
+  /**
+   * Evaluate the quadratic on a value.
+   */
+  public double evaluate(double x) {
+    return this.a*x*x  + this.b*x + this.c;
+  } // evaluate(double)
+
+  /**
+   * Find the smaller of the two roots.
+   */
+  public double smallerRoot() {
+    return 0;   // STUB
+  } // smallerRoot
+
+  /**
+   * Find the larger of the two roots.
+   */
+  public double largerRoot() {
+    return 0;   // STUB
+  } // largerRoot
+} // class Quadratic
